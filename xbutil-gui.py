bb0a1e551b29c73f8bc3aeb551564228e516e71a
Jim Wu <jimw567@gmail.com>
2020-12-31 09:26:01 -0800

    Added temperature plot

--- xbutil-gui.py
+++ xbutil-gui.py
@@ -7,7 +7,9 @@ import subprocess
 import json
 import shutil
 import datetime
-
+from apscheduler.schedulers.background import BackgroundScheduler
+import argparse
+import atexit
 
 # global variables
 status_codes = {
@@ -17,50 +19,84 @@ status_codes = {
     }
 }
 main_window = tk.Tk()
-figure_power = plt.Figure(figsize=(10, 5), dpi=100)
-plot_power = figure_power.add_subplot(111)
-canvas_power = FigureCanvasTkAgg(figure_power, main_window)
-canvas_power.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+main_window.title('Xilinx xbutil GUI')
+main_window.iconbitmap('resources/xbutil-icon.ico')
+figure_dpi = 100
+figure_hist = plt.Figure(figsize=(10, 5), dpi=figure_dpi)
+plot_hist = figure_hist.add_subplot(111)
+canvas_hist = FigureCanvasTkAgg(figure_hist, main_window)
+canvas_hist.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+plot_type = 'temp'  # valid values: power, temp, volt
 time_hist = []
 power_hist = []
+temp_hist = []
 refresh_interval = 10 # seconds between refresh
+scheduler = BackgroundScheduler(daemon=True)
 
-def get_xbutil_dump():
-    command = ['xbutil', 'dump']
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    xbutil_dump = p.stdout.read()
-    xbutil_dump_json = json.loads(xbutil_dump.decode('utf-8'))
+
+def get_xbutil_dump(json_file):
+    timestamp = datetime.datetime.now()
+    #print('DEBUG:', timestamp, 'get_xbutil_dump from', json_file)
+    if json_file is None:
+        command = ['xbutil', 'dump']
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        xbutil_dump = p.stdout.read()
+        xbutil_dump_json = json.loads(xbutil_dump.decode('utf-8'))
+    else:
+        with open(json_file, 'r') as fp:
+            xbutil_dump_json = json.load(fp)
+
     #time_hist.append(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-    time_hist.append(datetime.datetime.now().strftime("%H:%M:%S"))
+    time_hist.append(timestamp.strftime("%m/%d %H:%M"))
     power_hist.append(int(xbutil_dump_json['board']['physical']['power']))
-    #print('temp', json.dumps(xbutil_dump_json['board']['physical']['thermal']['fpga_temp'], indent=4))
+    temp_hist.append(float(xbutil_dump_json['board']['physical']['thermal']['fpga_temp']))
 
 
-def animate_power(iter):
+def animate_plot(iter):
     if iter % refresh_interval > 0:
         return
 
-    get_xbutil_dump()
-    plot_power.clear()
-    power_hist_dict = {'Time': time_hist[-10:], 'Power': power_hist[-10:]}
-    power_hist_df = DataFrame(power_hist_dict, columns=['Time', 'Power'])
-    power_hist_df = power_hist_df[['Time', 'Power']].groupby('Time').sum()
-    #print(iter, type(power_hist_df['Time']), type(power_hist_df['Power']))
-    power_hist_df.plot(kind='line', legend=False, ax=plot_power, color='r', marker='o', fontsize=10)
-    plot_power.set_title('Power History')
+    plot_hist.clear()
+    if plot_type == 'power':
+        y_hist_dict = {'time': time_hist[::refresh_interval], 'power': power_hist[::refresh_interval]}
+        y_hist_df = DataFrame(y_hist_dict, columns=['time', 'power'])
+        y_hist_df.plot(kind='line', legend=False, x='time', y='power', ax=plot_hist,
+                       color='r', marker='.', fontsize=10)
+        plot_hist.set_title('Power(w) History')
+    elif plot_type == 'temp':
+        y_hist_dict = {'time': time_hist[::refresh_interval], 'temp': temp_hist[::refresh_interval]}
+        y_hist_df = DataFrame(y_hist_dict, columns=['time', 'temp'])
+        y_hist_df.plot(kind='line', legend=False, x='time', y='temp', ax=plot_hist,
+                       color='r', marker='.', fontsize=10)
+        plot_hist.set_title('Temperature(C) History')
 
 
 def xbutil_gui_main():
+    global plot_type
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json-file', dest='json_file', default=None,
+                        help='Specify a JSON file for getting the data')
+    parser.add_argument('--plot-type', dest='plot_type', default='power',
+                        help='Specify plot type: power, temp, or volt')
+    args = parser.parse_args()
+    plot_type = args.plot_type
+    if args.json_file is None and shutil.which('xbutil') is None:
+        print(status_codes['XRT_NOT_SETUP']['message'])
+        exit(status_codes['XRT_NOT_SETUP']['code'])
+
+    # add a background task to get xbutil dump every second
+    scheduler.add_job(get_xbutil_dump, 'interval', [args.json_file], seconds=1)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown(wait=True))
+
     # refresh every 1 second
-    animation_power = animation.FuncAnimation(figure_power, animate_power, interval=1000)
+    animation_power = animation.FuncAnimation(figure_hist, animate_plot, interval=1000)
     main_window.mainloop()
 
 
 if __name__ == '__main__':
-    if shutil.which('xbutil') is None:
-        print(status_codes['XRT_NOT_SETUP']['message'])
-        exit(status_codes['XRT_NOT_SETUP']['code'])
-
+    # start the main GUI
     xbutil_gui_main()
 
 
